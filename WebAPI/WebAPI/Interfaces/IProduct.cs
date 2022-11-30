73782d9b389669e7d7f66944c1f9a78d61f73ebc
Thanh Phat <62296306+nguyen-thanhphat@users.noreply.github.com>
2022-11-30 19:14:42 +0700

    first commit

---
+++ WebAPI/WebAPI/Interfaces/IProduct.cs
@@ -0,0 +1,14 @@
+﻿using WebAPI.Model;
+
+namespace WebAPI.Interfaces
+{
+    public interface IProduct
+    {
+        ICollection<Product> GetProducts();
+        bool CreateProduct(Product product);
+        bool UpdateProduct(Product product);
+        bool DeleteProduct(Product product);
+
+        bool Save();
+    }
+}
---
+++ WebAPI/WebAPI/Repository/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using WebAPI.Interfaces;
+using WebAPI.Model;
+
+namespace WebAPI.Repository
+{
+    public class ProductRepository : IProduct
+    {
+        public bool CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
